9b053d390e91ee340048d6bf455fbd05cf9f1f13
Gabriele Bozzola <sbozzolator@gmail.com>
2024-09-23 19:43:03 -0700

    More filters

--- has_rse/blacklist.py
+++ has_rse/blacklist.py
@@ -9,5 +9,8 @@ blacklisted = {
     "https://people.cs.umass.edu/~brun/",
     "https://research.nd.edu/news-and-events/news/building-a-career-path-for-research-software-engineers/",
     "https://experts.illinois.edu/en/publications/research-software-engineering-in-2030",
-    "https://www.vanderbilt.edu/accre/accre-staff/"
+    "https://www.vanderbilt.edu/accre/accre-staff/",
+    "https://career.clemson.edu/jobs/ibm-software-engineer-ai-powered-automation-intern-2025/"
+    "https://www.cs.wisc.edu/2023/08/22/research-software-engineer-full-time/",
+    "https://experts.illinois.edu/en/publications/the-four-pillars-of-research-software-engineering"
 }
